261d1797d1843b9a8618943347f33f05d4670982
Hermes Zhang <chenhuiz@axis.com>
2020-05-14 20:52:16 +0800

    LICENSE: Change to MIT license
    Change-Id: Id2069ef2250c36fce5fd54fb11c7b1af75bf2ff2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     install_requires=['ffmpeg_python', 'av', 'numpy', 'matplotlib', 'pandas', 'ffmpeg'],
     classifiers=[
         "Development Status :: 3 - Alpha",
-        "License :: OSI Approved :: BSD License",
+        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Topic :: Utilities"
     ],
